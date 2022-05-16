638ea0a8853fa97395c31c3ea81970337ee2defb
Bushiran Ibrahim Witol <80077495+Bushiran@users.noreply.github.com>
2022-05-16 13:54:51 +0000

    Expense-Tracker
    Expense-tracker-app

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import  Header  from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+import Register from './components/Register';
+import Login from './components/Login';
+import Home from './components/Home';
+
+
+import { GlobalProvider } from "./context/GlobalState";
+ import "./App.css";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <Login />
+        <Home/>
+        <Register/>
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction/>
+      </div>
+    </GlobalProvider>
+
+   
+  );
+}
+
+export default App;
